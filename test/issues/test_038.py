96a1b0655b5b6379191fc1a472103c2e014a4be4
Ashley Sommer <ashleysommer@gmail.com>
2020-01-31 22:04:53 +1000

    Add actual test 38, rather than a copy of 36.

--- test/issues/test_038.py
+++ test/issues/test_038.py
@@ -1,53 +1,47 @@
 # -*- coding: utf-8 -*-
 #
 """
-https://github.com/RDFLib/pySHACL/issues/36
+https://github.com/RDFLib/pySHACL/issues/38
+
+I think this is duplicate of test test_029.py
 """
+from rdflib import Graph
 from pyshacl import validate
 
-shacl_file_text = """\
-{
-    "@context": {
-       "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-       "sh": "http://www.w3.org/ns/shacl#",
-       "schema": "http://schema.org/"
-    },
-    "@graph": [
-        {
-            "@id": "_:forceDatasetShape",
-            "@type": "sh:NodeShape",
-            "sh:targetNode": "schema:DigitalDocument",
-            "sh:property": [
-                {
-                    "sh:path": [
-                        {
-                            "sh:inversePath": [{
-                                "@id": "rdf:type",
-                                "@type": "@id"
-                             }]
-                        }
-                    ],
-                    "sh:minCount": 1
-                }
-            ]
-        }
-    ]
-}
-"""
+shapes = Graph()
+shapes.parse(data="""
+    @prefix sh: <http://www.w3.org/ns/shacl#> .
+    @prefix owl: <http://www.w3.org/2002/07/owl#> .
+    @prefix ex: <http://example.org/ns#> .
 
-data_file_text_jsonld = """\
-{
- 
-}
-"""
+    ex:Person
+          a owl:Class ;
+          a sh:NodeShape ;
+          sh:property ex:NameConstraint ;
+    .
 
-def test_036_jsonld():
-    res = validate(data_file_text_jsonld, shacl_graph=shacl_file_text,
-                   data_graph_format='json-ld', shacl_graph_format='json-ld',
-                   inference='rdfs', debug=True)
-    conforms, graph, string = res
+    ex:NameConstraint
+          a sh:PropertyShape ;
+          sh:path ex:name ;
+          sh:minCount 1 ;
+        .
+""",format="ttl")
+
+data = Graph()
+data.parse(data="""
+    @prefix ex: <http://example.org/ns#> .
+
+    ex:Bob
+          a ex:Person ;
+    .
+""",format="ttl")
+
+
+
+def test_038():
+    conforms, g, s = validate(data_graph=data, shacl_graph=shapes, inference='rdfs')
     assert not conforms
 
 
 if __name__ == "__main__":
-    test_036_jsonld()
+    test_038()
