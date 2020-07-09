d6af5ed34c1ef72e6c425ec359942a57611a4765
Ashley Sommer <ashleysommer@gmail.com>
2020-07-09 13:45:10 +1000

    bump to version 0.11.6

--- pyshacl/__init__.py
+++ pyshacl/__init__.py
@@ -3,6 +3,6 @@
 from pyshacl.validate import validate, Validator
 
 # version compliant with https://www.python.org/dev/peps/pep-0440/
-__version__ = '0.11.5'
+__version__ = '0.11.6'
 
 __all__ = ['validate', 'Validator', '__version__']
