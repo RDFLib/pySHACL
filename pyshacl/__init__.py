1af1b7144fb9ee11d8ea295cc5595a5071f76222
Ashley Sommer <ashleysommer@gmail.com>
2024-10-24 07:02:38 +1000

    Bump versions for v0.28.0 release

--- pyshacl/__init__.py
+++ pyshacl/__init__.py
@@ -7,7 +7,7 @@ from .shapes_graph import ShapesGraph
 from .validator import Validator
 
 # version compliant with https://www.python.org/dev/peps/pep-0440/
-__version__ = '0.27.0'
+__version__ = '0.28.0'
 # Don't forget to change the version number in pyproject.toml, Dockerfile, and CITATION.cff along with this one
 
 __all__ = ['validate', 'shacl_rules', 'Validator', 'RuleExpandRunner', '__version__', 'Shape', 'ShapesGraph']
